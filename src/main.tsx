27e995279b0607cd696f626e8f32e03c8fa25769
alexwood4real <akwood6@asu.edu>
2024-02-20 18:54:09 -0700

    Added Testnet
    We can change this to mainnet later

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    {/* AKW: If error persists, figure out why. Can change chain here */}
+    <ThirdwebProvider desiredChainId={ChainId.Goerli}>
     <App />
+    </ThirdwebProvider>
   </React.StrictMode>
 );
